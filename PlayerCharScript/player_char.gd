1fb3600bd91e0b776482efcdbb316afd0174fc15
Seiichi Aiko <seiichipaiko@iskolarngbayan.pup.edu.ph>
2025-12-08 18:58:35 +0800

    Added Player Sprite and Movement Physics
    I added movement physics and player sprite from youtube tutorials and free assets.

---
+++ PlayerCharScript/player_char.gd
@@ -0,0 +1,35 @@
+extends CharacterBody2D
+
+var max_speed = 100
+var last_direction = Vector2(1,0)
+
+func _physics_process(_delta):
+	var direction = Input.get_vector("MoveLeft", "MoveRight", "MoveUp", "MoveDown")
+	velocity = direction * max_speed 
+	move_and_slide()
+	
+	if direction.length() > 0:
+		last_direction = direction
+		play_walk_animation(direction)
+	else: 
+		play_idle_animation(last_direction)
+
+func play_walk_animation(direction):
+	if direction.x > 0:
+		$AnimatedSprite2D.play("WalkRight")
+	elif direction.x < 0:
+		$AnimatedSprite2D.play("WalkLeft")
+	elif direction.y > 0:
+		$AnimatedSprite2D.play("WalkDown")
+	elif direction.y < 0:
+		$AnimatedSprite2D.play("WalkUp")
+
+func play_idle_animation(direction):
+	if direction.x > 0:
+		$AnimatedSprite2D.play("IdleRight")
+	elif direction.x < 0:
+		$AnimatedSprite2D.play("IdleLeft")
+	elif direction.y > 0:
+		$AnimatedSprite2D.play("IdleDown")
+	elif direction.y < 0:
+		$AnimatedSprite2D.play("IdleUp")
