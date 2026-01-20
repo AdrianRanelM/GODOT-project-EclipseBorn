373471785e9448373d1300526dd9e0ab1fcc2a1a
Nikki Elisha Plaza <nikkiplaza24@gmail.com>
2026-01-20 11:45:26 +0800

    Added scene scripts, scene manager, and room tilemaps

--- Scripts/scene-scripts/scene_manager.gd
+++ Scripts/scene-scripts/scene_manager.gd
@@ -5,8 +5,11 @@ var player: CharacterBody2D
 var scene_dir_path = "res://scenes/"
 
 func change_scene(from, to_scene_name: String) -> void:
-	player = from.player
-	player.get_parent().remove_child(player)
+	if "player" in from:
+		player = from.player
+	
+	if player != null and player.get_parent() != null:
+		player.get_parent().remove_child(player)
 	
 	var full_path = scene_dir_path + to_scene_name + ".tscn"
 	from.get_tree().call_deferred("change_scene_to_file", full_path)
